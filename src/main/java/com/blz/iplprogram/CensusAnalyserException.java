51f7a06b7bd2075392bd8bcb8c5695b8c2d7b5c3
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2020-11-14 00:52:58 +0530

    Added:Code for Find Best Striker with best 4sN6s Cricketer(6sN4s).

---
+++ src/main/java/com/blz/iplprogram/CensusAnalyserException.java
@@ -0,0 +1,21 @@
+package com.blz.iplprogram;
+
+public class CensusAnalyserException extends Exception {
+
+	enum ExceptionType {
+		NO_SUCH_FILE, NO_SUCH_FIELD
+	}
+
+	ExceptionType exceptionType;
+
+	public CensusAnalyserException(String message, ExceptionType exceptionType) {
+		super(message);
+		this.exceptionType = exceptionType;
+	}
+
+	public CensusAnalyserException(String message, ExceptionType type, Throwable cause) {
+		super(message, cause);
+		this.exceptionType = type;
+	}
+
+}
