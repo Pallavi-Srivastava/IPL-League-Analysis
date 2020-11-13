08fda6676bdd666ac9ec5cf9e077c7eea9a07b8e
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2020-11-13 20:52:40 +0530

    Added:Code for Find Top Strike Rates.

---
+++ src/test/java/com/blz/iplprogram/IPLAnalyserTest.java
@@ -0,0 +1,54 @@
+package com.blz.iplprogram;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import com.google.gson.Gson;
+
+public class IPLAnalyserTest {
+
+	public static final String FILE_PATH = "E:\\Fellowship\\STS\\CensusAnalyserFile\\CorrectRuns.csv";
+	private static final double DELTA = 1e-15;
+
+	private static IPLAnalyser iplAnalyser;
+
+	@BeforeClass
+	public static void createcensusAnalyser() {
+		iplAnalyser = new IPLAnalyser();
+		System.out.println("Welcome to the IPL League Analyser Program.. ");
+	}
+
+	@Test
+	public void givenIPLInfo_ShouldReturnNumberOfRecords() throws CensusAnalyserException {
+		Assert.assertEquals(101, iplAnalyser.loadCSVData(FILE_PATH));
+	}
+
+	@Test
+	public void givenIPLInfo_ShouldReturnTopBattingAverages() {
+		String sortedCensusData = null;
+		try {
+			sortedCensusData = iplAnalyser.getTopBattingAverages(FILE_PATH);
+			IPLMostRunsCSV[] censusCSV = new Gson().fromJson(sortedCensusData, IPLMostRunsCSV[].class);
+			Assert.assertEquals(83.2, censusCSV[censusCSV.length - 1].avg, DELTA);
+		} catch (CensusAnalyserException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void givenIPLInfo_ShouldReturnTopStrikeRatings() {
+		String sortedCensusData = null;
+		try {
+			sortedCensusData = iplAnalyser.getTopStrikingRates(FILE_PATH);
+			IPLMostRunsCSV[] censusCSV = new Gson().fromJson(sortedCensusData, IPLMostRunsCSV[].class);
+			Assert.assertNotEquals(333.33, censusCSV[censusCSV.length - 1].avg, DELTA);
+		} catch (CensusAnalyserException e) {
+			e.printStackTrace();
+		}
+	}
+}
